15526271c2fc34cc5f10b0fb987158e83238082e
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-10-13 16:38:36 -0400

    Placeholder intro

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
 	return (
-		<div>
-			<h1 className='text-green-500'>Welcome</h1>
-		</div>
+		<>
+			<style jsx>{`
+				.main-bg {
+					background-color: #03254b;
+				}
+			`}</style>
+			<div className='main-bg flex flex-col h-screen w-full justify-center items-center'>
+				<h1 className='text-main text-6xl font-semibold heavy'>PolyKiosk</h1>
+				<p className='text-white mt-6 text-lg'>Instant Virtual Kiosks</p>
+			</div>
+		</>
 	)
 }
